0b0813458294739fb26db33d4b20f5da4c97e5ba
Gaurav <gauravp@zignuts.com>
2023-03-28 12:21:17 +0530

    added permissions method

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,9 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Models\RolePermission;
+use App\Models\UserRole;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -49,5 +52,10 @@ class User extends Authenticatable
     public function roles()
     {
         return $this->belongsToMany(Role::class,'user_roles','user_id','role_id');
+    }
+
+    public function permissions()
+    {
+        return $this->hasManyThrough(RolePermission::class,UserRole::class,'user_id','role_id');
     }
 }
