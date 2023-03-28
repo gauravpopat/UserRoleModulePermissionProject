2e613fd36668db1afedf89f7478dbcc04dce8eb9
Gaurav <gauravp@zignuts.com>
2023-03-28 19:00:53 +0530

    controller

---
+++ app/Http/Controllers/CourseController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CourseController extends Controller
+{
+    public function returnResponse()
+    {
+        ok('You have access for this page');
+    }
+}
