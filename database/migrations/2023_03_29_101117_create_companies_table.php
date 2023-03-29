ce7fc3a6e04e414d3b8b1097d007954363226390
Gaurav <gauravp@zignuts.com>
2023-03-29 17:23:34 +0530

    companies migration

---
+++ database/migrations/2023_03_29_101117_create_companies_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',255);
+            $table->enum('type',['it','non-it']);
+            $table->string('location',255);
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+};
