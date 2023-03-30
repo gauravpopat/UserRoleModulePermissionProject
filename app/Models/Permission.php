7c24062cc3b3173cf76a3437fdb2fb6d395e459d
Gaurav <gauravp@zignuts.com>
2023-03-30 16:46:29 +0530

    hasAccess()

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Permission extends Model
 {
@@ -17,31 +18,23 @@ class Permission extends Model
     }
 
     public function modules()
+
     {
-        return $this->belongsToMany(Module::class, 'module_permissions', 'permission_id', 'module_id')->withPivot(['list_access', 'create_access', 'view_access', 'update_access', 'delete_access']);
+        return $this->belongsToMany(Module::class, 'module_permissions', 'permission_id', 'module_id')->withPivot(['list_access', 'create_access', 'show_access', 'update_access', 'delete_access']);
     }
 
     public function hasAccess($module_code, $access)
     {
         foreach ($this->modules as $module) {
-
-
-            // $module->where('name', $module_code)->where($access, true)->first(); {
-            //     return true;
-            // }
-
-            // return false;
-
-            // if(strtolower($module->name) == strtolower($module_name)){
-            //     if($module->hasAccess($module_name,$access_name)){
-            //         return true;
-            //     };
-            // }
-
-            // if(strtolower($module->name) == strtolower($module_code) && $module->pivot->$access == true){
-            //     return true;
-            // }
-            // return false;
+            if ($module->where('name', $module_code)) {
+                if ($module->pivot->$access == true) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
         }
     }
 }
