eb7e87fb6ef7a61b363739bf86c8b02890ccb699
Gaurav <gauravp@zignuts.com>
2023-03-31 17:26:52 +0530

    Code formatting

--- app/Http/Controllers/ModulePermissionController.php
+++ app/Http/Controllers/ModulePermissionController.php
@@ -13,8 +13,8 @@ class ModulePermissionController extends Controller
         $validation = Validator::make($request->all(), [
             'permission_id'     => 'required|exists:permissions,id',
             'module_id'         => 'required|exists:modules,id',
-            'list_access'   =>  'required|in:1,0',
-            'show_access'   =>  'required|in:1,0',
+            'list_access'       => 'required|in:1,0',
+            'show_access'       => 'required|in:1,0',
             'create_access'     => 'required|in:1,0',
             'update_access'     => 'required|in:1,0',
             'delete_access'     => 'required|in:1,0',
@@ -36,6 +36,7 @@ class ModulePermissionController extends Controller
             'update_access' =>  'required|in:1,0',
             'delete_access' =>  'required|in:1,0'
         ]);
+
         if ($validation->fails())
             return error('Validation Error', $validation->errors(), 'validation');
 
@@ -46,7 +47,7 @@ class ModulePermissionController extends Controller
     public function show($id)
     {
         $modulePermission = ModulePermission::findOrFail($id);
-        return ok('Module Permisssion',$modulePermission);
+        return ok('Module Permisssion', $modulePermission);
     }
 
     public function delete($id)
